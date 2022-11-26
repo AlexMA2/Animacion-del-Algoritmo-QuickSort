95ff89ba04d42c492ae2a045fa976884664351ff
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-11-25 21:43:56 -0500

    Se elimino el titulo cambiante, se arreglo estilos

---
+++ src/animacion.js
@@ -0,0 +1,385 @@
+$(function () {
+  let bgColorPivote = "#DAAADB"; //morado
+  let bgColorI = "#F8A1A4"; // rojo
+  let bgColorJ = "#98E0AD"; //verde
+  let tiempoAvance = 800;
+  let tiempoIntercambio = 2000;
+  let quitarHeaderFooter = false;
+
+  let i = 0;
+  let j;
+  let piv = 0;
+  let longitud = 1;
+
+  let animI;
+  let animJ;
+  let timeoutIntercambio;
+  let reservaA;
+  let reservaB;
+
+  let estado = 0;
+
+  let pila = [];
+
+  let izq = 0;
+  let der;
+
+  let ejecI = false;
+  let ejecJ = false;
+  let inter = false;
+  let interVerdadero = false;
+
+  let dividido = true;
+
+  $("#btn-qs").on("click", function (ev) {
+    reiniciarDatosAnimacion();
+  });
+
+  $("#btn-play").on("click", function (ev) {
+    ev.preventDefault();
+    $("#btn-pause").prop("disabled", false);
+    if ($(".pantalla").length > 0 && $(".nodoHTML").length > 0) {
+      estado = 1;
+      $("#estado").text("Animación ejecutándose");
+      longitud = parseInt($("#cantidad").val());
+      der = longitud - 1;
+      setBackgrounds();
+      pila.push([izq, der]);
+
+      iniciarIntervalos();
+      $(this).prop("disabled", true);
+    }
+  });
+
+  $("#btn-pause").on("click", function (ev) {
+    ev.preventDefault();
+    $("#estado").text("Animación pausada");
+    $(this).prop("disabled", true);
+    $("#btn-restart").prop("disabled", false);
+    if (estado == 1) {
+      clearInterval(animI);
+      clearInterval(animJ);
+      clearTimeout(timeoutIntercambio);
+    }
+    estado = 2;
+  });
+
+  $("#btn-restart").on("click", function (ev) {
+    ev.preventDefault();
+    $("#estado").text("Animación reiniciada");
+    $("#btn-pause").prop("disabled", false);
+    $(this).prop("disabled", true);
+    if (estado == 2) {
+      estado = 1;
+
+      if (inter && !interVerdadero) {
+        intercambiarValores(reservaA, reservaB);
+      } else if (ejecJ) {
+        animJ = setInterval(reducirJ, tiempoAvance);
+      } else if (ejecI) {
+        animI = setInterval(incrementarI, tiempoAvance);
+      }
+    }
+  });
+
+  const iniciarIntervalos = () => {
+    if (estado == 1) {
+      if (pila.length > 0 || !dividido) {
+        if (dividido) {
+          dividido = false;
+          let siguiente = pila.pop();
+          i = siguiente[0];
+          piv = siguiente[0];
+          izq = siguiente[0];
+          j = siguiente[1];
+          der = siguiente[1];
+          $(".nodoHTML").css({
+            backgroundColor: "#fff",
+          });
+        }
+        if (!ejecI) {
+          ejecI = true;
+          animI = setInterval(incrementarI, tiempoAvance);
+        }
+      } else {
+        //Acabo de ordenar
+        $("#estado").text("Animación terminada");
+        $(".nodoHTML").css({
+          backgroundColor: "#fff",
+        });
+        reiniciarDatosAnimacion();
+        estado = 0;
+      }
+    }
+  };
+
+  const setBackgrounds = () => {
+    if (i > 0) {
+      $(".nodoHTML")
+        .eq(i - 1)
+        .css("backgroundColor", "#fff");
+    }
+    $(".nodoHTML").eq(i).css("backgroundColor", bgColorI);
+
+    $(".nodoHTML").eq(piv).css("backgroundColor", bgColorPivote);
+    if (j < longitud) {
+      $(".nodoHTML")
+        .eq(j + 1)
+        .css("backgroundColor", "#fff");
+    }
+    $(".nodoHTML").eq(j).css("backgroundColor", bgColorJ);
+  };
+
+  const incrementarI = () => {
+    interVerdadero = false;
+    ejecI = true;
+    if (i < j && getValor(i) <= getValor(piv)) {
+      i++;
+      setBackgrounds();
+
+      if (getValor(i) > getValor(piv)) {
+        clearInterval(animI);
+        ejecI = false;
+        if (!ejecJ) {
+          ejecJ = true;
+          animJ = setInterval(reducirJ, tiempoAvance);
+        }
+      }
+    } else if (i >= j) {
+      clearInterval(animI);
+      ejecI = false;
+      intercambiar(piv, j);
+    }
+  };
+
+  const reducirJ = () => {
+    interVerdadero = false;
+    ejecJ = true;
+    if (getValor(j) > getValor(piv)) {
+      j--;
+
+      setBackgrounds();
+
+      if (getValor(j) <= getValor(piv)) {
+        clearInterval(animJ);
+        ejecJ = false;
+        if (i < j) {
+          intercambiar(i, j);
+        } else {
+          intercambiar(piv, j);
+        }
+      }
+    } else {
+      clearInterval(animJ);
+      ejecJ = false;
+      intercambiar(i, j);
+    }
+  };
+
+  const intercambiar = (a, b) => {
+    inter = true;
+    interVerdadero = false;
+    if (estado == 1) {
+      let nodo1 = getNodo(a);
+      let nodo2 = getNodo(b);
+
+      let posicion1 = nodo1.position();
+      let posicion2 = nodo2.position();
+
+      nodo1.css("position", "relative");
+      nodo2.css("position", "relative");
+      mover(
+        nodo1,
+        posicion2.left - posicion1.left,
+        posicion2.top - posicion1.top
+      );
+      mover(
+        nodo2,
+        posicion1.left - posicion2.left,
+        posicion1.top - posicion2.top
+      );
+      inter = true;
+      reservaA = a;
+      reservaB = b;
+      timeoutIntercambio = setTimeout(() => {
+        intercambiarValores(a, b);
+      }, tiempoIntercambio);
+    }
+  };
+
+  const intercambiarValores = (a, b) => {
+    inter = false;
+    interVerdadero = true;
+    reservaA = -1;
+    reservaB = -1;
+    let aux = getValor(a);
+    setValor(a, getValor(b));
+    setValor(b, aux);
+    if (i < j) {
+      iniciarIntervalos();
+    } else {
+      //El i y el j se cruzaron, se movio el pivote
+      dividido = true;
+      dividir();
+    }
+  };
+
+  const mover = (nodo, x, y) => {
+    nodo.animate(
+      {
+        opacity: 0.3,
+        left: x + "px",
+        top: y + "px",
+      },
+      tiempoIntercambio,
+      function () {
+        nodo.css({
+          opacity: 1,
+          position: "static",
+          top: 0,
+          left: 0,
+        });
+      }
+    );
+  };
+
+  const dividir = () => {
+    if (der - (j + 1) > 0) {
+      pila.push([j + 1, der]);
+    }
+    if (j - 1 - izq > 0) {
+      pila.push([izq, j - 1]);
+    }
+
+    iniciarIntervalos();
+  };
+
+  // const opacarZona = (desde , hasta) => {
+  //
+  //     for (let iterator = desde; iterator <= hasta; iterator++) {
+  //
+  //         $(".nodoHTML").eq(iterator).css("opacity", 1);
+  //     }
+  //
+
+  // }
+
+  const getValor = (x) => {
+    return parseInt($(".nodoHTML").eq(x).children(".nodo-valor").text());
+  };
+
+  const setValor = (x, nuevoValor) => {
+    $(".nodoHTML").eq(x).children(".nodo-valor").text(nuevoValor);
+  };
+
+  const getNodo = (x) => {
+    return $(".nodoHTML").eq(x);
+  };
+
+  $(".btn-sorts").on("click", function (ev) {
+    ev.preventDefault();
+    reiniciarDatosAnimacion();
+  });
+
+  const reiniciarDatosAnimacion = () => {
+    if (estado === 1) {
+      clearInterval(animI);
+      clearInterval(animJ);
+      i = 0;
+      izq = 0;
+      j = 0;
+      piv = 0;
+      dividido = true;
+      pila = [];
+      estado = 0;
+      $("#btn-play").prop("disabled", false);
+    }
+  };
+
+  // Configuarciones
+
+  $("#guardar-config").on("click", function () {
+    let valor = parseInt($("#t-intercambio").val());
+    if (!isNaN(valor)) {
+      tiempoIntercambio = valor;
+    }
+
+    valor = parseInt($("#t-recorrido").val());
+    if (!isNaN(valor)) {
+      tiempoAvance = valor;
+    }
+
+    if ($("#no-repes").prop("checked")) {
+      quitarHeaderFooter = true;
+      $("header").slideUp("fast");
+      $("footer").css("display", "none");
+    } else {
+      quitarHeaderFooter = false;
+      $("header").slideDown("fast");
+      $("footer").css("display", "block");
+    }
+
+    valor = $("#bg-pivote").val();
+
+    if (esColor(valor)) {
+      bgColorPivote = valor;
+    }
+
+    valor = $("#bg-i").val();
+
+    if (esColor(valor)) {
+      bgColorI = valor;
+    }
+
+    valor = $("#bg-j").val();
+
+    if (esColor(valor)) {
+      bgColorJ = valor;
+    }
+  });
+
+  $("#reestablecer-config").on("click", function () {
+    bgColorPivote = "#DAAADB"; //morado
+    bgColorI = "#F8A1A4"; // rojo
+    bgColorJ = "#98E0AD"; //verde
+    tiempoAvance = 800;
+    tiempoIntercambio = 2000;
+    quitarHeaderFooter = false;
+    $("#t-intercambio").val("2000");
+    $("#t-recorrido").val("800");
+    $("#no-repes").prop("checked", false);
+    $("#bg-pivote").val("#DAAADB");
+    $("#bg-i").val("#F8A1A4");
+    $("#bg-j").val("#98E0AD");
+  });
+
+  const esColor = (color) => {
+    if (color.length == 7 && color.charAt(0) == "#") {
+      color = color.toUpperCase();
+      for (let index = 1; index < color.length; index++) {
+        if (
+          color.charAt(index) !== "0" &&
+          color.charAt(index) !== "1" &&
+          color.charAt(index) !== "2" &&
+          color.charAt(index) !== "3" &&
+          color.charAt(index) !== "4" &&
+          color.charAt(index) !== "5" &&
+          color.charAt(index) !== "6" &&
+          color.charAt(index) !== "7" &&
+          color.charAt(index) !== "8" &&
+          color.charAt(index) !== "9" &&
+          color.charAt(index) !== "A" &&
+          color.charAt(index) !== "B" &&
+          color.charAt(index) !== "C" &&
+          color.charAt(index) !== "D" &&
+          color.charAt(index) !== "E" &&
+          color.charAt(index) !== "F"
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+});
---
+++ src/main.js
@@ -0,0 +1,288 @@
+$(function () {
+  let cantidadNodos = 0;
+  let veloInicial = 40;
+  let velocidad = veloInicial;
+  const aceleracion = 0.2;
+  let toLeft;
+  let toRight;
+
+  const anchoNodo = 41;
+
+  $("#cantidad").on("input", function (ev) {
+    ev.preventDefault();
+
+    if (ev.target.value > cantidadNodos) {
+      $("#btn-qs").prop("disabled", true);
+    }
+
+    if (ev.target.value === "") {
+      crearArray(0);
+    } else if (ev.target.value > 200) {
+      crearArray(200);
+      $("#cantidad").val(200);
+    } else {
+      crearArray(ev.target.value);
+    }
+  });
+
+  $("#cantidad").on("keydown", function (ev) {
+    if (ev.keyCode === 190 || ev.keyCode === 107 || ev.keyCode === 109) {
+      ev.preventDefault();
+    }
+  });
+
+  $("#izquierda").on("mousedown", function (ev) {
+    ev.preventDefault();
+    toLeft = setInterval(() => {
+      moverCarrusel("left");
+    }, 100);
+  });
+
+  $("#izquierda").on("mouseleave", function (ev) {
+    ev.preventDefault();
+    clearInterval(toLeft);
+    velocidad = veloInicial;
+  });
+
+  $("#izquierda").on("mouseup", function (ev) {
+    ev.preventDefault();
+    clearInterval(toLeft);
+    velocidad = veloInicial;
+  });
+
+  $("#derecha").on("mousedown", function (ev) {
+    ev.preventDefault();
+    toRight = setInterval(() => {
+      moverCarrusel("right");
+    }, 100);
+  });
+
+  $("#derecha").on("mouseleave", function (ev) {
+    ev.preventDefault();
+    clearInterval(toRight);
+    velocidad = veloInicial;
+  });
+
+  $("#derecha").on("mouseup", function (ev) {
+    ev.preventDefault();
+    clearInterval(toRight);
+    velocidad = veloInicial;
+  });
+
+  $("#alea-cantidad").on("click", function (ev) {
+    ev.preventDefault();
+    let random = Math.round(Math.random() * 100 + 3);
+    $("#cantidad").val(random);
+    crearArray(random);
+  });
+
+  $("#alea-valores").on("click", function (ev) {
+    ev.preventDefault();
+    if (!isNaN(parseInt($("#cantidad").val()))) {
+      $(".valores input").each(function () {
+        $(this).val(Math.round(Math.random() * 200 - 100));
+      });
+      $("#btn-qs").prop("disabled", false);
+    }
+  });
+
+  let inputsLlenos = Array();
+
+  $(".valores").on("input", "input", function (ev) {
+    ev.preventDefault();
+    if (estanCompletosLosNodos()) {
+      $("#btn-qs").prop("disabled", false);
+    }
+    if (ev.target.value > 200) {
+      crearArray(200);
+      $("#cantidad").val(200);
+    }
+  });
+
+  $(".valores").on("keydown", "input", function (ev) {
+    let indexActual = $(this).index();
+    let longitudDeInputs = $(".valores input").length - 1;
+
+    if (ev.keyCode == 39) {
+      //derecha
+      if (indexActual < longitudDeInputs) {
+        $(".valores input")
+          .eq(indexActual + 1)
+          .focus();
+      }
+    } else if (ev.keyCode == 37) {
+      //izquierda
+      if (indexActual > 0) {
+        $(".valores input")
+          .eq(indexActual - 1)
+          .focus();
+      }
+    }
+  });
+
+  let oculto = false;
+
+  tippy("#popup-btn-listo", {
+    content: "Click aqui para ver la animacion",
+  });
+
+  $("#btn-qs").on("click", function (ev) {
+    toggleConfiguraciones();
+    toggleAnimaciones();
+    crearListaDeNodos();
+
+    if (!oculto) {
+      $("body").css("overflow", "hidden");
+      tippy("#popup-btn-listo", {
+        content: "Click aqui para ver la animación",
+      });
+    } else {
+      $("body").css("overflow", "auto");
+      tippy("#popup-btn-listo", {
+        content: "Click aqui para volver a la configuración",
+      });
+    }
+  });
+
+  const toggleConfiguraciones = () => {
+    if (!oculto) {
+      oculto = true;
+      $(".ocultar").slideUp("fast", "linear", function () {
+        $("#btn-qs").html(
+          "No Listo <i class='fa fa-check-circle text-right'></i>"
+        );
+        $("#btn-qs").addClass("btn-danger");
+        $("#btn-qs").removeClass("btn-success");
+      });
+    } else {
+      oculto = false;
+      $(".ocultar").slideDown("fast", "linear", function () {
+        $("#btn-qs").html(
+          "Listo <i class='fa fa-check-circle text-right'></i>"
+        );
+        $("#btn-qs").addClass("btn-success");
+        $("#btn-qs").removeClass("btn-danger");
+      });
+    }
+  };
+
+  const toggleAnimaciones = () => {
+    if (oculto) {
+      $(".animacion").css("display", "block");
+      $("#estado").text("Animación por empezar");
+    } else {
+      $(".animacion").css("display", "none");
+      $("#estado").text("");
+    }
+  };
+
+  const crearArray = (numero) => {
+    if (numero > cantidadNodos) {
+      let nodos = numero - cantidadNodos;
+
+      for (let i = 0; i < nodos; i++) {
+        crearInput();
+      }
+      cantidadNodos = numero;
+    } else if (numero < cantidadNodos) {
+      while (numero < cantidadNodos) {
+        if ($(".valores input")[numero] == undefined) {
+          break;
+        }
+        $(".valores input")[numero].remove();
+      }
+      cantidadNodos = numero;
+    }
+  };
+
+  let LISTACREADA = false;
+
+  const crearNodoHTML = (valor) => {
+    let nodoHTML =
+      "<div class='nodoHTML'><p class='nodo-valor'> " + valor + " </p></div>";
+    return nodoHTML;
+  };
+
+  const crearPantalla = (id) => {
+    let pantallaHTML = "<div class='pantalla' id='p" + id + "'></div>";
+    $("#pantallas").append(pantallaHTML);
+  };
+
+  const crearListaDeNodos = () => {
+    if (LISTACREADA) {
+      LISTACREADA = false;
+      $("#pantallas").empty();
+    }
+
+    let contador = 1;
+    crearPantalla(contador);
+    let pantalla = $("#p" + contador);
+    const MAXELEMENTOS = calcularMaximoPorPantalla();
+    let maximun = MAXELEMENTOS;
+
+    $(".valores input").each(function (index, element) {
+      let nodo = crearNodoHTML($(element).val());
+      pantalla.append(nodo);
+
+      if (index >= maximun) {
+        contador += 1;
+        maximun += MAXELEMENTOS;
+        crearPantalla(contador);
+        pantalla = $("#p" + contador);
+      }
+    });
+
+    LISTACREADA = true;
+  };
+
+  const calcularMaximoPorPantalla = () => {
+    let anchoPantalla = $(".pantalla").innerWidth();
+    return Math.floor(anchoPantalla / anchoNodo) - 1;
+  };
+
+  $(window).resize(function () {
+    if (LISTACREADA) {
+      LISTACREADA = false;
+      $("#pantallas").empty();
+      toggleConfiguraciones();
+      toggleAnimaciones();
+    }
+  });
+
+  const estanCompletosLosNodos = () => {
+    let retorno = true;
+
+    $(".valores input").each(function (index, element) {
+      if ($(element).val() === "") {
+        retorno = false;
+        return false;
+      }
+    });
+
+    if (retorno == false) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const crearInput = () => {
+    let clon = $("#cantidad").clone();
+
+    clon.val("");
+    clon.attr("id", "");
+    clon.attr("class", "nodo-value");
+    clon.attr("min", "");
+    clon.appendTo($(".valores"));
+  };
+
+  const moverCarrusel = (direccion) => {
+    if (direccion === "left") {
+      $(".valores").scrollLeft($(".valores").scrollLeft() - velocidad);
+      velocidad += aceleracion;
+    } else if (direccion === "right") {
+      $(".valores").scrollLeft($(".valores").scrollLeft() + velocidad);
+      velocidad += aceleracion;
+    }
+  };
+});
